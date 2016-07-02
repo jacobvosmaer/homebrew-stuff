54f1a8ab616743957cf4a5ed6423c19016039e93
Jacob Vosmaer <contact@jacobvosmaer.nl>
2016-07-02 17:19:46 +0200

    Import mpd.rb from 96cbce0

---
+++ mpd.rb
@@ -0,0 +1,140 @@
+class Mpd < Formula
+  desc "Music Player Daemon"
+  homepage "https://www.musicpd.org/"
+  url "https://www.musicpd.org/download/mpd/0.19/mpd-0.19.16.tar.xz"
+  sha256 "58ecbff4b07ec42a6f31bdbbb3c3eb51317978e4a0ea3071f6ae6127848a03d6"
+
+  bottle do
+    cellar :any
+    sha256 "c7f59dd212cc121bc18ae35218a1391577f73caab75756013f67399447e77f37" => :el_capitan
+    sha256 "1633dc1e733fe9d9c0028421e001d0f4b9e1794d361aefbb9485422f19ce3964" => :yosemite
+    sha256 "7282b0ef5a5f10d6f8d97ec83ffef8828c502524ec4b001e6ae9d0d1ec0c483c" => :mavericks
+  end
+
+  head do
+    url "git://git.musicpd.org/master/mpd.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  option "with-wavpack", "Build with wavpack support (for .wv files)"
+  option "with-lastfm", "Build with last-fm support (for experimental Last.fm radio)"
+  option "with-lame", "Build with lame support (for MP3 encoding when streaming)"
+  option "with-two-lame", "Build with two-lame support (for MP2 encoding when streaming)"
+  option "with-flac", "Build with flac support (for Flac encoding when streaming)"
+  option "with-libvorbis", "Build with vorbis support (for Ogg encoding)"
+  option "with-yajl", "Build with yajl support (for playing from soundcloud)"
+  option "with-opus", "Build with opus support (for Opus encoding and decoding)"
+  option "with-libmodplug", "Build with modplug support (for decoding modules supported by MODPlug)"
+
+  deprecated_option "with-vorbis" => "with-libvorbis"
+
+  depends_on "pkg-config" => :build
+  depends_on "boost" => :build
+  depends_on "glib"
+  depends_on "libid3tag"
+  depends_on "sqlite"
+  depends_on "libsamplerate"
+  depends_on "icu4c"
+
+  needs :cxx11
+
+  depends_on "libmpdclient"
+  depends_on "ffmpeg" # lots of codecs
+  # mpd also supports mad, mpg123, libsndfile, and audiofile, but those are
+  # redundant with ffmpeg
+  depends_on "fluid-synth"              # MIDI
+  depends_on "faad2"                    # MP4/AAC
+  depends_on "wavpack" => :optional     # WavPack
+  depends_on "libshout" => :optional    # Streaming (also pulls in Vorbis encoding)
+  depends_on "lame" => :optional        # MP3 encoding
+  depends_on "two-lame" => :optional    # MP2 encoding
+  depends_on "flac" => :optional        # Flac encoding
+  depends_on "jack" => :optional        # Output to JACK
+  depends_on "libmms" => :optional      # MMS input
+  depends_on "libzzip" => :optional     # Reading from within ZIPs
+  depends_on "yajl" => :optional        # JSON library for SoundCloud
+  depends_on "opus" => :optional        # Opus support
+  depends_on "libvorbis" => :optional
+  depends_on "libnfs" => :optional
+  depends_on "mad" => :optional
+  depends_on "libmodplug" => :optional  # MODPlug decoder
+
+  def install
+    # mpd specifies -std=gnu++0x, but clang appears to try to build
+    # that against libstdc++ anyway, which won't work.
+    # The build is fine with G++.
+    ENV.libcxx
+
+    system "./autogen.sh" if build.head?
+
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --enable-bzip2
+      --enable-ffmpeg
+      --enable-fluidsynth
+      --enable-osx
+      --disable-libwrap
+    ]
+
+    args << "--disable-mad" if build.without? "mad"
+    args << "--disable-curl" if MacOS.version <= :leopard
+
+    args << "--enable-zzip" if build.with? "libzzip"
+    args << "--enable-lastfm" if build.with? "lastfm"
+    args << "--disable-lame-encoder" if build.without? "lame"
+    args << "--disable-soundcloud" if build.without? "yajl"
+    args << "--enable-vorbis-encoder" if build.with? "libvorbis"
+    args << "--enable-nfs" if build.with? "libnfs"
+    args << "--enable-modplug" if build.with? "libmodplug"
+
+    system "./configure", *args
+    system "make"
+    ENV.j1 # Directories are created in parallel, so let's not do that
+    system "make", "install"
+
+    (etc/"mpd").install "doc/mpdconf.example" => "mpd.conf"
+  end
+
+  plist_options :manual => "mpd"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/mpd</string>
+            <string>--no-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    pid = fork do
+      exec "#{bin}/mpd --stdout --no-daemon --no-config"
+    end
+    sleep 2
+
+    begin
+      assert_match "OK MPD", shell_output("curl localhost:6600")
+    ensure
+      Process.kill "SIGINT", pid
+      Process.wait pid
+    end
+  end
+end
